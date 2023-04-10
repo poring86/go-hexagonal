37972ba129be66ea3833a9a80e5039e2a36a6608
Matheus Lino <matheuslino86@gmail.com.br>
2023-04-10 16:55:54 -0400

    feat: :sparkles: Test service mock

---
+++ application/product_service_test.go
@@ -0,0 +1,25 @@
+package application_test
+
+import (
+	"testing"
+
+	"github.com/poring86/go-hexagonal/application"
+	mock_application "github.com/poring86/go-hexagonal/application/mocks"
+	"github.com/stretchr/testify/require"
+
+	"github.com/golang/mock/gomock"
+)
+
+func TestProductService_Get(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	product := mock_application.NewMockProductInterface(ctrl)
+	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
+	persistence.EXPECT().Get(gomock.Any()).Return(product, nil).AnyTimes()
+	service := application.ProductService{Persistence: persistence}
+
+	result, err := service.Get("abc")
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+
+}
