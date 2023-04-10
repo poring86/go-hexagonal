1ec132b6f2dd8c2bd5029ff5bc34cf60f4b79fed
Matheus Lino <matheuslino86@gmail.com.br>
2023-04-10 12:30:53 -0400

    feat: :sparkles: Test product is valid

--- application/product_test.go
+++ application/product_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/poring86/go-hexagonal/application"
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/require"
 )
 
@@ -34,4 +35,27 @@ func TestProduct_Disable(t *testing.T) {
 	product.Price = 10
 	err = product.Disable()
 	require.Equal(t, "The price must be zero in order to have the product disabled", err.Error())
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "INVALID"
+	_, err = product.IsValid()
+	require.Equal(t, "the status must be enabled or disabled", err.Error())
+
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "the price must be greater or equal zero", err.Error())
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/poring86/go-hexagonal
 
 go 1.16
 
-require github.com/stretchr/testify v1.8.2
+require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
+	github.com/stretchr/testify v1.8.2
+)
