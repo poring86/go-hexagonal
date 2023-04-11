9e17c32b7ba2ac449bb1a3eaeff054a87106077c
Matheus Lino <matheuslino86@gmail.com.br>
2023-04-11 11:28:45 -0400

    feat: :sparkles: Test Product Db get

--- adapters/db/product_test.go
+++ adapters/db/product_test.go
@@ -1,8 +1,12 @@
-package db
+package db_test
 
 import (
 	"database/sql"
 	"log"
+	"testing"
+
+	"github.com/poring86/go-hexagonal/adapters/db"
+	"github.com/stretchr/testify/require"
 )
 
 var Db *sql.DB
@@ -11,6 +15,7 @@ func setUp() {
 	// Db :=
 	Db, _ = sql.Open("sqlite3", ":memory:")
 	createTable(Db)
+	createProduct(Db)
 }
 
 func createTable(db *sql.DB) {
@@ -20,4 +25,24 @@ func createTable(db *sql.DB) {
 		log.Fatal(err.Error())
 	}
 	stmt.Exec()
+}
+
+func createProduct(db *sql.DB) {
+	insert := `insert into products values("abc", "Product Test", 0, "disabled")`
+	stmt, err := db.Prepare(insert)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	stmt.Exec()
+}
+
+func TestProductDb_Get(t *testing.T) {
+	setUp()
+	defer Db.Close()
+	productDb := db.NewProductDb(Db)
+	product, err := productDb.Get("abc")
+	require.Nil(t, err)
+	require.Equal(t, "Product Test", product.GetName())
+	require.Equal(t, 0.0, product.GetPrice())
+	require.Equal(t, "disabled", product.GetStatus())
 }
